41e8aa339bc59e2a155cc8f1c1ece649c8a5bd6a
HamidAbbasi-R <106018114+HamidAbbasi-R@users.noreply.github.com>
2025-02-19 13:23:18 +0000

    Update maximum number of stock price paths in slider to 10,000

--- Black-Scholes.py
+++ Black-Scholes.py
@@ -203,7 +203,7 @@ def plot_paths(paths, times=None, N_show=10):
 
 # Create an instance of the StochasticProcesses class
 sp = StochasticProcesses()
-paths_st = st.slider("Number of Stock Price Paths", min_value=100, max_value=12000, value=1000, step=10)
+paths_st = st.slider("Number of Stock Price Paths", min_value=100, max_value=10_000, value=1000, step=10)
 paths_GBM = sp.geometric_brownian_motion(S0=S_max, mu=r, sigma=sigma, T=T, dt=1/252, simulations=paths_st)
 paths_BM = sp.brownian_motion(S0=S_max, mu=r, sigma=sigma, T=T, dt=1/252, simulations=paths_st)
 
